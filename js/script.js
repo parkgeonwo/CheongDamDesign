d78df95840b276f1178a1e7cb5bd766a2a58c1e0
Geonwoo Park <parkkeonwo@naver.com>
2022-11-29 23:18:01 +0900

    221129 23:17 version 0.2

--- js/script.js
+++ js/script.js
@@ -14,10 +14,9 @@ function losFocus(id) {
   var qty_name = "txt_qty_" + pIndex;	//수량 컬럼의 ID
   var tot_name = "tot_amt_" + pIndex; //총 금액 컬럼의 ID
 
-
   var qty = document.getElementById(qty_name).value;	//해당 수량컬럼의 값을 qty 에 저장
   var pri = document.getElementById(pri_name).value;	//해당 가격컬럼의 값을 pri 에 저장
-  var tot_amt = qty.replace(",", "") * pri.replace(",", ""); //수량, 또는 금액 값에 ","" 가 입력되있을 때 오류를 방지하기 위해 .replace 로  "," 제거
+  var tot_amt = qty.replace(/,/g, "") * pri.replace(/,/g, ""); //수량, 또는 금액 값에 ","" 가 입력되있을 때 오류를 방지하기 위해 .replace 로  "," 제거
 
   document.getElementById(tot_name).innerHTML = tot_amt.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");	//총 금액컬럼에 계산된 금액 입력. 3자리마다 "," 입력.
 }
@@ -148,68 +147,35 @@ function stringNumberToInt(stringNumber){
 
 
 
+///////////////////////////////// 저장된값 불러와서 표시하기
+function page1_local_data_display() {
+  // 저장값 불러오기
+  var json_data = JSON.parse(localStorage.getItem("price"));
+  
+  document.getElementById("page1_price_1").innerText = json_data['폐기물처리,철거']
+  document.getElementById("page1_price_2").innerText = json_data['문공사']
+  document.getElementById("page1_price_3").innerText = json_data['창호공사']
+  document.getElementById("page1_price_4").innerText = json_data['도배,바닥공사']
+  document.getElementById("page1_price_5").innerText = json_data['필름공사']
+  document.getElementById("page1_price_6").innerText = json_data['타일,욕실공사']
+  document.getElementById("page1_price_7").innerText = json_data['목공사']
+  document.getElementById("page1_price_8").innerText = json_data['전기공사']
+  document.getElementById("page1_price_9").innerText = json_data['확장공사']
+  document.getElementById("page1_price_10").innerText = json_data['철물공사']
+  document.getElementById("page1_price_11").innerText = json_data['기타공사']
+  document.getElementById("page1_price_12").innerText = json_data['도장공사']
+  document.getElementById("page1_price_13").innerText = json_data['가구공사']
+  document.getElementById("page1_price_14").innerText = json_data['이윤 및 공과잡비']
+  document.getElementById("page1_price_15").innerText = json_data['소계']
+  document.getElementById("page1_price_16").innerText = json_data['부가세']
+  document.getElementById("page1_price_17").innerText = json_data['합계']
+}
+
+
+
 
 
 ////////////////////////////////////////          page2            ////////////////////////////////////////////////
-
-
-
-// //////////////////////////////////////////////////// row 합계 구하기
-// function losFocus_part(id) {
-//   //ID 형식 :  txt + "_" + OOO(축약어) + "_" + index 
-//   //따라서 앞에 고정 문자열의 개수는 8개(txt_ooo_)
-//   var pDefault_lenght = 8;
-  
-//   //전달받은 ID의 끝자리 Index를 추출해서 
-//   //동일 Index를 가지고 있는 ID의 이름에 Index 를 붙여줌.
-//   var pIndex = String(id.substring(pDefault_lenght, id.length));
-//     var tot_name = "tot_amt_" + pIndex; //총 금액 컬럼의 ID
-
-//   // var tot_amt_value = document.getElementById(tot_name).value; // 각줄의 tot_amt 값 가져오기
-
-//   // if (isNaN(tot_amt_value)) {  // nan이면 0으로
-//   //   tot_amt_value = 0
-//   // }
-
-//   // objName 체크 
-//   if ( document.getElementById("tot_name")  == null ) { 
-//     var tot_amt_value = 0
-//   }
-//   else{
-//     var tot_amt_value = document.getElementById(tot_name).value;
-//   }
-//   console.log(tot_amt_value)
-
-
-//   // var tot_amt = tot_amt_value.replace(",", "");  // ',' 제거
-  
-//   let sum = 0
-
-//   if (pIndex>0 && pIndex<15){
-//     var tot_name_part = "tot_amt_15"
-//     for(let i = 0; i < 14; i++){
-//       let value = stringNumberToInt(table.rows[i].cells[5].innerText);
-//       sum += value
-//     }
-
-//   }
-
-//   document.getElementById(tot_name_part).innerHTML = sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");	//총 금액컬럼에 계산된 금액 입력. 3자리마다 "," 입력.
-//   // document.getElementById(tot_name).innerHTML = tot_amt.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");	//총 금액컬럼에 계산된 금액 입력. 3자리마다 "," 입력.
-// }
-
-// function showKeyCode(event, name) {
-//   event = event || window.event;
-//   var keyID = (event.which) ? event.which : event.keyCode;
-//   if ((keyID >= 48 && keyID <= 57) || (keyID >= 96 && keyID <= 105) || keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39 || keyID == 188) {
-//     return;
-//   } else {
-//     return false;
-//   }
-// }
-
-
-
 
 
 ///////////////////////////////////////////////  page2 총합계구하기
@@ -228,7 +194,7 @@ function calcSumPage2() {
   calcSumPart(index=11)
   calcSumPart(index=12)
   calcSumPart(index=13)
-
+  calcSumPart(index=14)
 
   // table element 찾기
   const table = document.getElementById('page2_table_id');
@@ -237,7 +203,7 @@ function calcSumPage2() {
   let total_sum = 0;
 
   // part_sum_array
-  var part_sum_array = [0, 15, 19, 23, 29 , 41, 53, 60, 70, 73, 76, 82, 85, 99] // i값
+  var part_sum_array = [15, 19, 23, 29 , 41, 53, 60, 70, 73, 76, 82, 85, 99,103] // i값
 
   for(let i = 0; i < table.rows.length-1; i++)  {  // 마지막행 제외하고 계산
     if (part_sum_array.includes(i)){
@@ -247,16 +213,48 @@ function calcSumPage2() {
       if (isNaN(value)) {
           value = 0
         }
-      console.log("value: "+value)
+      console.log("part_sum_value: "+value)
       total_sum += value
       // sum += parseInt(table.rows[i].cells[5].innerText);
     }
   }
 
+  // 부가세가 안더해진 값 저장하기
+  var total_sum_without_vat = total_sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+  // 부가세 값 가져와서 더해주기
+  var vat = document.getElementById("txt_pri_105").value;	//해당 가격컬럼의 값을 vat 에 저장
+  var vat_replace = vat.replace(/,/g, "");
+  total_sum += stringNumberToInt(vat_replace)
+
   // 합계 출력
   total_sum = total_sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");	//총 금액컬럼에 계산된 금액 입력. 3자리마다 "," 입력.
   document.getElementById('tot_amt_last_page2').innerText = total_sum;
 
+  // 가격 데이터 page간 전달을 위한 json
+  var price = {
+      "폐기물처리,철거" : table.rows[part_sum_array[0]].cells[5].innerText,
+      "문공사" : table.rows[part_sum_array[1]].cells[5].innerText,
+      "창호공사" : table.rows[part_sum_array[2]].cells[5].innerText,
+      "도배,바닥공사" : table.rows[part_sum_array[3]].cells[5].innerText,
+      "필름공사" : table.rows[part_sum_array[4]].cells[5].innerText,
+      "타일,욕실공사" : table.rows[part_sum_array[5]].cells[5].innerText,
+      "목공사" : table.rows[part_sum_array[6]].cells[5].innerText,
+      "전기공사" : table.rows[part_sum_array[7]].cells[5].innerText,
+      "확장공사" : table.rows[part_sum_array[8]].cells[5].innerText,
+      "철물공사" : table.rows[part_sum_array[9]].cells[5].innerText,
+      "기타공사" : table.rows[part_sum_array[10]].cells[5].innerText,
+      "도장공사" : table.rows[part_sum_array[11]].cells[5].innerText,
+      "가구공사" : table.rows[part_sum_array[12]].cells[5].innerText,
+      "이윤 및 공과잡비" : table.rows[part_sum_array[13]].cells[5].innerText,
+      "소계" :total_sum_without_vat,
+      "부가세" : vat,
+      "합계" : total_sum,
+  };
+
+  // Json Object를 저장하기
+  localStorage.setItem("price", JSON.stringify(price));
+  
 }
 
 function stringNumberToInt(stringNumber){
@@ -326,11 +324,16 @@ function calcSumPart(index) {
     var part_array = [87,88,89,90,91,92,93,94,95,96,97,98]
     var tot_amt_id = 'tot_amt_100'
   }
+  if (index==14){
+    var part_array = [101,102]
+    var tot_amt_id = 'tot_amt_104'
+  }
 
 
   for(let i = 0; i < table.rows.length-1; i++)  {  // 마지막행 제외하고 계산
     if (part_array.includes(i)){
       let value = stringNumberToInt(table.rows[i].cells[5].innerText);
+      
       // 만약 value가 Nan 이라면 0으로 처리
       // sum 시켜주기
       if (isNaN(value)) {
@@ -346,15 +349,294 @@ function calcSumPart(index) {
   total_sum = total_sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");	//총 금액컬럼에 계산된 금액 입력. 3자리마다 "," 입력.
   document.getElementById(tot_amt_id).innerText = total_sum;
 
+
+
+
+  var all_part_array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,
+    17,18,
+    21,22,
+    25,26,27,28,
+    31,32,33,34,35,36,37,38,39,40,
+    43,44,45,46,47,48,49,50,51,52,
+    55,56,57,58,59,
+    62,63,64,65,66,67,68,69,
+    72,
+    75,
+    78,79,80,81,
+    84,
+    87,88,89,90,91,92,93,94,95,96,97,98,
+    101,102
+  ]
+
+  // console.log(table.rows[i].cells[4].innerText);
+
+  // 가격 데이터 page간 전달을 위한 json // 수량 데이터
+  var all_part_num_data = {};
+
+  for(let i = 0; i < table.rows.length-1; i++)  {  // 마지막행 제외하고 계산
+    if (all_part_array.includes(i)){
+      // let value = stringNumberToInt(table.rows[i].cells[0].innerText);
+      
+      var qty_name = "txt_qty_" + String(i+1);	//수량 컬럼의 ID
+      var inputValue = document.getElementById(qty_name).value;
+
+      // console.log(value)
+      all_part_num_data[i+1] = inputValue
+    }
+  }
+
+  // Json Object를 저장하기
+  localStorage.setItem("all_part_num_data", JSON.stringify(all_part_num_data));
+
+  // 가격 데이터 page간 전달을 위한 json 
+  var all_part_price_data = {};
+
+  for(let i = 0; i < table.rows.length-1; i++)  {  // 마지막행 제외하고 계산
+    if (all_part_array.includes(i)){
+      // let value = stringNumberToInt(table.rows[i].cells[0].innerText);
+      
+      var pri_name = "txt_pri_" + String(i+1);	//수량 컬럼의 ID
+      var inputValue = document.getElementById(pri_name).value;
+
+      // console.log(value)
+      all_part_price_data[i+1] = inputValue
+    }
+  }
+
+  // Json Object를 저장하기
+  localStorage.setItem("all_part_price_data", JSON.stringify(all_part_price_data));
+
+
 } 
 
 
 
+/////////////////////////////// 전에 계산했던 데이터 불러오기
+function DataloadPage2() {
+  // table element 찾기
+  const table = document.getElementById('page2_table_id');
+
+  var all_part_array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,
+    17,18,
+    21,22,
+    25,26,27,28,
+    31,32,33,34,35,36,37,38,39,40,
+    43,44,45,46,47,48,49,50,51,52,
+    55,56,57,58,59,
+    62,63,64,65,66,67,68,69,
+    72,
+    75,
+    78,79,80,81,
+    84,
+    87,88,89,90,91,92,93,94,95,96,97,98,
+    101,102
+  ]
+
+  var all_part_num_data= JSON.parse(localStorage.getItem("all_part_num_data"));
+  var all_part_price_data= JSON.parse(localStorage.getItem("all_part_price_data"));
+
+  for(let i = 0; i < table.rows.length-1; i++)  {  // 마지막행 제외하고 계산
+    if (all_part_array.includes(i)){
+      // let value = stringNumberToInt(table.rows[i].cells[0].innerText);
+      
+      var qty_name = "txt_qty_" + String(i+1);	//수량 컬럼의 ID
+      document.getElementById(qty_name).value = all_part_num_data[i+1];
+
+      var pri_name = "txt_pri_" + String(i+1);	//단가 컬럼의 ID
+      document.getElementById(pri_name).value = all_part_price_data[i+1];
+
+      CalcRow(i+1)
+    }
+  }
+
+  calcSumPage2()  
+
+}
+
+//////////////////////////////////////////////////// row 합계 구하기
+function CalcRow(id) {
+  //ID 형식 :  txt + "_" + OOO(축약어) + "_" + index 
+  //따라서 앞에 고정 문자열의 개수는 8개(txt_ooo_)
+  var pDefault_lenght = 8;
+  
+  //전달받은 ID의 끝자리 Index를 추출해서 
+  //동일 Index를 가지고 있는 ID의 이름에 Index 를 붙여줌.
+  var pIndex = String(id);
+  
+  var pri_name = "txt_pri_" + pIndex;	//가격 컬럼의 ID
+  var qty_name = "txt_qty_" + pIndex;	//수량 컬럼의 ID
+  var tot_name = "tot_amt_" + pIndex; //총 금액 컬럼의 ID
+
+  var qty = document.getElementById(qty_name).value;	//해당 수량컬럼의 값을 qty 에 저장
+  var pri = document.getElementById(pri_name).value;	//해당 가격컬럼의 값을 pri 에 저장
+  var tot_amt = qty.replace(/,/g, "") * pri.replace(/,/g, ""); //수량, 또는 금액 값에 ","" 가 입력되있을 때 오류를 방지하기 위해 .replace 로  "," 제거
+
+  document.getElementById(tot_name).innerHTML = tot_amt.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");	//총 금액컬럼에 계산된 금액 입력. 3자리마다 "," 입력.
+}
 
 
 
+////////////////////////////////////////  select 함수
 
 
+function artwallValue(){
+  var value = document.getElementById('artwall');
+  var text = value.options[value.selectedIndex].text;
+  if( text == 'MDF' ){
+    document.getElementById('txt_pri_4').value = "120,000";
+  }
+  if( text == '대리석돌' ){
+    document.getElementById('txt_pri_4').value = "220,000";
+  }
+}
+
+
+function doorValue(){
+  var value = document.getElementById('door');
+  var text = value.options[value.selectedIndex].text;
+  if( text == '문' ){
+    document.getElementById('txt_pri_12').value = "34,000";
+  }
+  if( text == '문틀' ){
+    document.getElementById('txt_pri_12').value = "13,000";
+  }
+}
+
+
+function middoorValue(){
+  var value = document.getElementById('middoor');
+  var text = value.options[value.selectedIndex].text;
+  if( text == '3연동슬림' ){
+    document.getElementById('txt_pri_18').value = "1,050,000";
+  }
+  if( text == '스윙도어' ){
+    document.getElementById('txt_pri_18').value = "12,500,000";
+  }
+  if( text == '프리미엄슬라이딩' ){
+    document.getElementById('txt_pri_18').value = "1,200,000";
+  }
+}
+
+
+function roomdoorValue(){
+  var value = document.getElementById('roomdoor');
+  var text = value.options[value.selectedIndex].text;
+  if( text == 'ABS도어' ){
+    document.getElementById('txt_pri_19').value = "350,000";
+  }
+  if( text == '문짝' ){
+    document.getElementById('txt_pri_19').value = "145,000";
+  }
+  if( text == '문틀' ){
+    document.getElementById('txt_pri_19').value = "195,000";
+  }
+}
+function tunningdoorValue(){
+  var value = document.getElementById('tunningdoor');
+  var text = value.options[value.selectedIndex].text;
+  if( text == '' ){
+    document.getElementById('txt_pri_22').value = "";
+  }
+  if( text == '' ){
+    document.getElementById('txt_pri_22').value = "";
+  }
+  if( text == '' ){
+    document.getElementById('txt_pri_22').value = "";
+  }
+  if( text == '' ){
+    document.getElementById('txt_pri_22').value = "";
+  }
+  if( text == '' ){
+    document.getElementById('txt_pri_22').value = "";
+  }
+}
+
+function balconydoorValue(){
+  var value = document.getElementById('balconydoor');
+  var text = value.options[value.selectedIndex].text;
+  if( text == '' ){
+    document.getElementById('txt_pri_23').value = "";
+  }
+  if( text == '' ){
+    document.getElementById('txt_pri_23').value = "";
+  }
+  if( text == '' ){
+    document.getElementById('txt_pri_23').value = "";
+  }
+  if( text == '' ){
+    document.getElementById('txt_pri_23').value = "";
+  }
+  if( text == '' ){
+    document.getElementById('txt_pri_23').value = "";
+  }
+}
+function roomValue(){
+  var value = document.getElementById('room');
+  var text = value.options[value.selectedIndex].text;
+  if( text == '강마루' ){
+    document.getElementById('txt_pri_29').value = "115,000";
+  }
+  if( text == '장판2.2t' ){
+    document.getElementById('txt_pri_29').value = "55,000";
+  }
+  if( text == '데코타일' ){
+    document.getElementById('txt_pri_29').value = "48,000";
+  }
+}
+function bathtubValue(){
+  var value = document.getElementById('bathtub');
+  var text = value.options[value.selectedIndex].text;
+  if( text == 'SMC' ){
+    document.getElementById('txt_pri_48').value = "270,000";
+  }
+  if( text == '조적욕조' ){
+    document.getElementById('txt_pri_48').value = "520,000";
+  }
+}
+function faucetValue(){
+  var value = document.getElementById('faucet');
+  var text = value.options[value.selectedIndex].text;
+  if( text == '거위목' ){
+    document.getElementById('txt_pri_92').value = "140,000";
+  }
+  if( text == '슈티에' ){
+    document.getElementById('txt_pri_92').value = "270,000";
+  }
+}
+function hoodValue(){
+  var value = document.getElementById('hood');
+  var text = value.options[value.selectedIndex].text;
+  if( text == '빌트인' ){
+    document.getElementById('txt_pri_93').value = "90,000";
+  }
+  if( text == '침니' ){
+    document.getElementById('txt_pri_93').value = "280,000";
+  }
+  if( text == '아일랜드후드' ){
+    document.getElementById('txt_pri_93').value = "490,000";
+  }
+}
+function opencloseValue(){
+  var value = document.getElementById('openclose');
+  var text = value.options[value.selectedIndex].text;
+  if( text == '여닫이' ){
+    document.getElementById('txt_pri_96').value = "124,000";
+  }
+  if( text == '슬라이딩한샘' ){
+    document.getElementById('txt_pri_96').value = "185,000";
+  }
+}
+
+
+
+// 19 , roomdoor = "<select id='roomdoor' onchange='roomdoorValue()' style='width:90%'><option>ABS도어</option><option>문짝</option><option>문틀</option></select>"
+// 22, tunningdoor = "<select id='tunningdoor' onchange='tunningdoorValue()' style='width:90%'><option>LG</option><option>대우</option><option>현대</option><option>KCC</option><option>한샘</option></select>"
+// 23, balconydoor = "<select id='balconydoor' onchange='balconydoorValue()' style='width:90%'><option>LG</option><option>대우</option><option>현대</option><option>KCC</option><option>한샘</option></select>"
+// 29, room = "<select id='room' onchange='roomValue()' style='width:90%'><option>강마루</option><option>장판2.2t</option><option>데코타일</option></select>"
+// 48, bathtub = "<select id='bathtub' onchange='bathtubValue()' style='width:90%'><option>SMC</option><option>조적욕조</option></select>"
+// 92, faucet = "<select id='faucet' onchange='faucetValue()' style='width:90%'><option>거위목</option><option>슈티에</option></select>"
+// 93, hood = "<select id='hood' onchange='hoodValue()' style='width:90%'><option>빌트인</option><option>침니</option><option>아일랜드후드</option></select>"
+// 96, openclose = "<select id='openclose' onchange='opencloseValue()' style='width:90%'><option>여닫이</option><option>슬라이딩한샘</option></select>"
 
 
 
